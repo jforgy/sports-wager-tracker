df5d036ad4cd78ef8e6a8d9d0a601204d21ee478
Josh Forgy <JForgy@ilfb.org>
2025-06-17 14:56:39 -0500

    Fix secret_key_base issue for production deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,10 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
+  
+  # DISABLE MASTER KEY REQUIREMENT
   config.require_master_key = false
+  
   config.public_file_server.enabled = true
   config.assets.compile = true
   config.assets.digest = false
@@ -16,6 +19,9 @@ Rails.application.configure do
 
   # Disable SSL for Railway
   config.force_ssl = false
+
+  # Generate secret key base from environment variable or random
+  config.secret_key_base = ENV['SECRET_KEY_BASE'] || SecureRandom.hex(64)
 
   # Logging
   config.log_level = :info
